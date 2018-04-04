230e47fa471d8e0a155608c59953c9abfe3b601a
a-rtur <arturfulgonixavier@gmail.com>
2018-04-04 12:41:43 -0300

    Utilização do Consign para agilizar e facilitar o carregamento das routes

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var express = require('./config/server.js');
-var routeNoticias = require('./app/routes/noticias.js')(express);
-var routeHome = require('./app/routes/home.js')(express);
-var routeAdmin = require('./app/routes/admin.js')(express);
 
 express.listen(3000, function(){
     console.log('Tudo OK! Servidor online.');
--- config/server.js
+++ config/server.js
@@ -1,4 +1,6 @@
 var express = require('express')();
+var consign = require('consign');
 express.set('view engine', 'ejs');
 express.set('views', './app/views');
+consign().include('./app/routes').into(express);
 module.exports = express;
