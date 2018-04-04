7263fdf16cc71ea1a892d66ce96566814cd0e6fe
a-rtur <arturfulgonixavier@gmail.com>
2018-04-04 13:17:57 -0300

    Adicionada mensagem de erro caso a conexão com o banco falhe.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -2,7 +2,12 @@ module.exports = function(express) {
     express.get("/noticias", function(req, res) {
         var connection = require('../../config/mysql_connection.js');
         connection.query('select * from noticias', function(error, result) {
-            res.render("noticias/noticias.ejs", {noticias : result});
+            if (error == null) {
+                res.render("noticias/noticias.ejs", {noticias : result});
+            }
+            else {
+                res.end('Algo deu errado!');
+            }
         });
     });
 };
