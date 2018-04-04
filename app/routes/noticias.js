4f81b43197d9e7e962334821f4d166bb4a100d8e
a-rtur <arturfulgonixavier@gmail.com>
2018-04-04 14:27:10 -0300

    Criação da model para notícia

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,14 @@
 module.exports = function(express) {
     express.get("/noticias", function(req, res) {
         var connection = require('../../config/mysql_connection.js');
-        connection.query('select * from noticias', function(error, result) {
+        var noticiasModel = express.app.models.noticiasModel;
+        noticiasModel.getNoticias(connection, function(error, result) {
             if (error == null) {
                 res.render("noticias/noticias.ejs", {noticias : result});
             }
             else {
                 res.end('Algo deu errado!');
+                console.log('Algo de errado não está certo:' + error + '...Fim');
             }
         });
     });
--- config/server.js
+++ config/server.js
@@ -2,5 +2,5 @@ var express = require('express')();
 var consign = require('consign');
 express.set('view engine', 'ejs');
 express.set('views', './app/views');
-consign().include('./app/routes').into(express);
+consign().include('./app/routes').then('./app/models').into(express);
 module.exports = express;
